5edf194ba73a825f68e6b9eccbd67db9c18f6f37
José Pineda <epineda.amezquita@gmail.com>
2020-03-19 21:01:22 -0600

    Revert "Primer Commit"
    Para regresar los archivos de datos
    
    This reverts commit 51cbb934f672ed989c0006e50f72c3e01e5b37a0.

--- update.bat
+++ update.bat
@@ -1,26 +1,10 @@
 mensaje=$1
 
-echo "################# INICIO ################# "
-echo "Estado"
-echo ""
 git status
-echo ""
-
-echo "Agregar"
 git add *
 git add .i*
-echo ""
-
-echo "Enviar Commit"
 git commit -m "$mensaje"
-echo ""
-
 echo "Enviando a github..."
-echo ""
 sleep 2
 echo "..."
-echo ""
-
-git push -u ImpotarYAnalizarDatosEnPython master
-echo ""
-echo "################# FIN ################# "
+git push -u ProcesadoDeImagenes master
