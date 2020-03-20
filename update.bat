51cbb934f672ed989c0006e50f72c3e01e5b37a0
José Pineda <epineda.amezquita@gmail.com>
2020-03-19 20:45:37 -0600

    Primer Commit

--- update.bat
+++ update.bat
@@ -1,10 +1,26 @@
 mensaje=$1
 
+echo "################# INICIO ################# "
+echo "Estado"
+echo ""
 git status
+echo ""
+
+echo "Agregar"
 git add *
 git add .i*
+echo ""
+
+echo "Enviar Commit"
 git commit -m "$mensaje"
+echo ""
+
 echo "Enviando a github..."
+echo ""
 sleep 2
 echo "..."
-git push -u ProcesadoDeImagenes master
+echo ""
+
+git push -u ImpotarYAnalizarDatosEnPython master
+echo ""
+echo "################# FIN ################# "
